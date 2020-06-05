344cf199becdebb48ee10f20f95ad420b8f6db70
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-04 23:47:20 -0300

    MainActivity and MainViewModel refactored to get the data from repository, build.gradle was also updated

--- app/src/main/java/dev/lucasvillaverde/recipeapp/MainActivity.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/MainActivity.kt
@@ -1,31 +1,41 @@
 package dev.lucasvillaverde.recipeapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import dev.lucasvillaverde.recipeapp.data.local.entities.MealEntity
-import dev.lucasvillaverde.recipeapp.data.remote.RetrofitService
-import dev.lucasvillaverde.recipeapp.data.remote.responses.MealResponse
-import dev.lucasvillaverde.recipeapp.data.remote.services.MealService
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel = MainViewModel()
+    private val mainViewModel: MainViewModel by lazy {
+        val application = requireNotNull(application) {
+            "Application must not be null!"
+        }
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainViewModel.getRandomMeal().observe(this, Observer {
-            txtMain.text = it.name
+        mainViewModel.meals.observe(this, Observer { it ->
+            Log.d("MEAL_ACTIVITY", "MEAL OBSERVER, ITEM CHANGED")
+            updateMealTitle(it)
         })
 
         btnRefreshMeal.setOnClickListener(View.OnClickListener {
-            mainViewModel.getRandomMeal()
+            mainViewModel.refreshDataFromRepository()
         })
+    }
+
+    private fun updateMealTitle(mealList: List<MealEntity>) {
+        txtMain.text = ""
+        mealList.map {
+            txtMain.text = txtMain.text.toString().plus(" / ").plus(it.name)
+
+        }
     }
 }
--- app/src/main/java/dev/lucasvillaverde/recipeapp/MainViewModel.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/MainViewModel.kt
@@ -1,16 +1,39 @@
 package dev.lucasvillaverde.recipeapp
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import dev.lucasvillaverde.recipeapp.data.local.entities.MealEntity
+import dev.lucasvillaverde.recipeapp.data.local.getDatabase
 import dev.lucasvillaverde.recipeapp.data.repositories.MealRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    val mealRepository = MealRepository()
+    val mealRepository = MealRepository(getDatabase(application))
+    val meals = mealRepository.meals
+    private var _networkError = MutableLiveData(false)
 
-    fun getRandomMeal(): LiveData<MealEntity> {
-        return mealRepository.getRandomMeal()
+    init {
+        refreshDataFromRepository()
+    }
+
+    fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                mealRepository.refreshMeals()
+                _networkError.value = false
+            } catch (error: IOException) {
+                if (meals.value.isNullOrEmpty()){
+                    _networkError.value = true
+                    Log.d("ERROR", "ERROR: " + error.localizedMessage)
+                }
+            }
+        }
     }
 
 }
