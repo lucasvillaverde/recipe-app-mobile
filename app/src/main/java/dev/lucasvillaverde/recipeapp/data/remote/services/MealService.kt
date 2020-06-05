adaabc4fe40c604e88bf9c240d4c26c0a12fb0b4
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-04 23:45:38 -0300

    Added repository pattern from DB

--- app/src/main/java/dev/lucasvillaverde/recipeapp/data/remote/services/MealService.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/data/remote/services/MealService.kt
@@ -1,13 +1,14 @@
 package dev.lucasvillaverde.recipeapp.data.remote.services
 
-import dev.lucasvillaverde.recipeapp.data.local.entities.MealEntity
 import dev.lucasvillaverde.recipeapp.data.remote.responses.MealResponse
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface MealService {
 
     @GET("random.php")
-    fun getRandomMealData() : Call<MealResponse>
+    suspend fun getRandomMealData(): MealResponse
+
+    @GET("random.php")
+    suspend fun getMeals(): MealResponse
 
 }
--- app/src/main/java/dev/lucasvillaverde/recipeapp/data/repositories/MealRepository.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/data/repositories/MealRepository.kt
@@ -2,37 +2,24 @@ package dev.lucasvillaverde.recipeapp.data.repositories
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import dev.lucasvillaverde.recipeapp.data.Network
+import dev.lucasvillaverde.recipeapp.data.local.MealDatabase
 import dev.lucasvillaverde.recipeapp.data.local.entities.MealEntity
-import dev.lucasvillaverde.recipeapp.data.remote.RetrofitService
-import dev.lucasvillaverde.recipeapp.data.remote.responses.MealResponse
-import dev.lucasvillaverde.recipeapp.data.remote.services.MealService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.await
 
-class MealRepository {
+class MealRepository(private val mealDatabase: MealDatabase) {
 
     private val meal = MutableLiveData<MealEntity>()
-    private val mealService = RetrofitService.retrofitService.create(MealService::class.java)
 
-    fun getRandomMeal() : LiveData<MealEntity> {
-        val randomMealCall = mealService.getRandomMealData()
-        randomMealCall.enqueue(object: Callback<MealResponse> {
-            override fun onFailure(call: Call<MealResponse>, t: Throwable) {
-                t.printStackTrace()
-            }
+    val meals: LiveData<List<MealEntity>> = mealDatabase.mealDAO.getMeals()
 
-            override fun onResponse(call: Call<MealResponse>, response: Response<MealResponse>) {
-                if (response.isSuccessful){
-                    response.body()?.mealList?.let {
-                        meal.value = it[0]
-                    }
-                }
-            }
-
-        })
-
-        return meal
+    suspend fun refreshMeals() {
+        withContext(Dispatchers.IO) {
+            val meals = Network.mealService.getRandomMealData()
+            mealDatabase.mealDAO.insertAll(meals.mealList)
+        }
     }
 
 }
