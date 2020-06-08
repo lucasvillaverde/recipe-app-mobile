b2641f7becff975804a95a215b4f1cd9a2fe92d3
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-07 21:35:12 -0300

    Refactored main xml style to Material Card

--- app/src/main/java/dev/lucasvillaverde/recipeapp/MainActivity.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
 import dev.lucasvillaverde.recipeapp.data.local.entities.MealEntity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,19 +24,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mainViewModel.meals.observe(this, Observer { it ->
             Log.d("MEAL_ACTIVITY", "MEAL OBSERVER, ITEM CHANGED")
-            updateMealTitle(it)
         })
 
         btnRefreshMeal.setOnClickListener(View.OnClickListener {
-            mainViewModel.refreshDataFromRepository()
+            mainViewModel.getRandomMeal()?.let { meal ->
+                updateMealCard(meal)
+            }
         })
     }
 
-    private fun updateMealTitle(mealList: List<MealEntity>) {
-        txtMain.text = ""
-        mealList.map {
-            txtMain.text = txtMain.text.toString().plus(" / ").plus(it.name)
-
-        }
+    private fun updateMealCard(meal: MealEntity) {
+        Picasso.get().load(meal.thumb).into(imgMeal);
+        txtMealTitle.text = meal.name
+        txtMealDescription.text = meal.region
+        txtInstructions.text = meal.instructions
     }
 }
