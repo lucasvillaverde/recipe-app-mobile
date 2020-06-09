3bc341528e0777edf288964801f75cab6d111797
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-08 21:53:58 -0300

    Created MealDetails layout and view model

--- app/src/main/java/dev/lucasvillaverde/recipeapp/ui/MealDetailsActivity.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/ui/MealDetailsActivity.kt
@@ -1,12 +1,36 @@
 package dev.lucasvillaverde.recipeapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
 import dev.lucasvillaverde.recipeapp.R
+import dev.lucasvillaverde.recipeapp.data.local.entities.MealEntity
+import dev.lucasvillaverde.recipeapp.viewmodels.MealDetailsViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_meal_details.*
 
-class MealDetails : AppCompatActivity() {
+class MealDetailsActivity : AppCompatActivity() {
+
+    private val mealDetailsViewModel: MealDetailsViewModel by lazy {
+        val application = requireNotNull(application) {
+            "Application must not be null!"
+        }
+        ViewModelProvider(this).get(MealDetailsViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meal_details)
+    }
+
+    private fun updateMealCard(meal: MealEntity) {
+        Picasso.get().load(meal.thumb).into(imgMeal);
+        txtMealTitle.text = meal.name
+        txtMealDescription.text = meal.region
+        txtInstructions.text = meal.instructions
     }
 }
--- app/src/main/java/dev/lucasvillaverde/recipeapp/viewmodels/MainViewModel.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/viewmodels/MainViewModel.kt
@@ -1,12 +1,10 @@
-package dev.lucasvillaverde.recipeapp
+package dev.lucasvillaverde.recipeapp.viewmodels
 
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import dev.lucasvillaverde.recipeapp.data.local.entities.MealEntity
 import dev.lucasvillaverde.recipeapp.data.local.getDatabase
 import dev.lucasvillaverde.recipeapp.data.repositories.MealRepository
 import kotlinx.coroutines.launch
@@ -15,27 +13,8 @@ import java.io.IOException
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     val mealRepository = MealRepository(getDatabase(application))
-    val meals = mealRepository.meals
-    private var _networkError = MutableLiveData(false)
+    private val meals = mealRepository.meals
 
-    init {
-        refreshDataFromRepository()
-    }
-
-    fun refreshDataFromRepository() {
-        viewModelScope.launch {
-            try {
-                mealRepository.refreshMeals()
-                _networkError.value = false
-            } catch (error: IOException) {
-                if (meals.value.isNullOrEmpty()){
-                    _networkError.value = true
-                    Log.d("ERROR", "ERROR: " + error.localizedMessage)
-                }
-            }
-        }
-    }
-
-    fun getRandomMeal() = mealRepository.meals.value?.random()
+    fun getMeals() = meals
 
 }
