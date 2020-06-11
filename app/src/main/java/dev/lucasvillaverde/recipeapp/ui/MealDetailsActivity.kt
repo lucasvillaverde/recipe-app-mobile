a5dfa77bd6da35a5b5e956b4289d6ace23bedd91
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-11 00:49:27 -0300

    Added youtube player to meal details

--- app/src/main/java/dev/lucasvillaverde/recipeapp/ui/MealDetailsActivity.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/ui/MealDetailsActivity.kt
@@ -1,9 +1,13 @@
 package dev.lucasvillaverde.recipeapp.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.squareup.picasso.Picasso
 import dev.lucasvillaverde.recipeapp.R
 import dev.lucasvillaverde.recipeapp.data.local.entities.MealEntity
@@ -22,7 +26,7 @@ class MealDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meal_details)
-
+        lifecycle.addObserver(youtubePlayerView)
         val mealId = intent.getIntExtra("MEAL_ID", 0)
         mealDetailsViewModel.getMealById(mealId).observe(this, Observer {
             updateMealCard(it)
@@ -31,9 +35,35 @@ class MealDetailsActivity : AppCompatActivity() {
     }
 
     private fun updateMealCard(meal: MealEntity) {
-        Picasso.get().load(meal.thumb).into(imgMeal);
+        loadMediaUI(meal)
         txtMealTitle.text = meal.name
         txtMealDescription.text = meal.region
         txtInstructions.text = meal.instructions
     }
+
+    private fun loadMediaUI(meal: MealEntity) {
+        mediaLoader.visibility = View.VISIBLE
+
+        val youtubeVideoID = meal.getYoutubeVideoID()
+        if (youtubeVideoID != null) {
+            imgMeal.visibility = View.GONE
+            youtubePlayerView.visibility = View.VISIBLE
+            youtubePlayerView.addYouTubePlayerListener(youtubeListener(youtubeVideoID))
+        } else {
+            Picasso.get().load(meal.thumb).into(imgMeal);
+            youtubePlayerView.visibility = View.GONE
+            imgMeal.visibility = View.VISIBLE
+        }
+
+        mediaLoader.visibility = View.GONE
+    }
+
+    private fun youtubeListener(youtubeVideoID: String) = object: AbstractYouTubePlayerListener() {
+        @Override
+        override fun onReady(youTubePlayer: YouTubePlayer) {
+            super.onReady(youTubePlayer)
+            youTubePlayer.cueVideo(youtubeVideoID, 0F)
+        }
+    }
+
 }
