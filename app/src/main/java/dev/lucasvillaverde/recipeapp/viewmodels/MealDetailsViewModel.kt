400310e18f043732d711ff78d19e5968e22760a2
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-06-20 22:57:38 -0300

    Removed unused code

--- app/src/main/java/dev/lucasvillaverde/recipeapp/viewmodels/MealDetailsViewModel.kt
+++ app/src/main/java/dev/lucasvillaverde/recipeapp/viewmodels/MealDetailsViewModel.kt
@@ -6,6 +6,7 @@ import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import dev.lucasvillaverde.recipeapp.data.local.getDatabase
@@ -16,26 +17,6 @@ import java.io.IOException
 class MealDetailsViewModel(application: Application) : AndroidViewModel(application) {
 
     val mealRepository = MealRepository(getDatabase(application))
-    private val meals = mealRepository.meals
-    private var _networkError = MutableLiveData(false)
-
-    init {
-        refreshDataFromRepository()
-    }
-
-    fun refreshDataFromRepository() {
-        viewModelScope.launch {
-            try {
-                mealRepository.refreshMeals()
-                _networkError.value = false
-            } catch (error: IOException) {
-                if (meals.value.isNullOrEmpty()) {
-                    _networkError.value = true
-                    Log.d("ERROR", "ERROR: " + error.localizedMessage)
-                }
-            }
-        }
-    }
 
     fun hasInternet(): Boolean {
         val cm = getApplication<Application>()
