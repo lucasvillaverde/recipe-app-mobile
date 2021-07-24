40a4b95a2832a51e3baf06e814f70c614cf7c699
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2021-07-24 16:54:04 -0300

    build(gradle): implementing gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class){
+    delete(rootProject.buildDir)
+}
+extra.apply{
+    set("minSdkVersion", 21)
+    set("targetSdkVersion", 30)
+}
